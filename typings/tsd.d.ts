a34b375e45e1edb1134ed0d12997703bdadd185f
Martin Poelstra <martin@beryllium.net>
2016-02-27 18:41:00 +0100

    Update to TS 1.8.2, `moduleResolution: node`-typings, update ts-promise.

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,4 +1,1 @@
-// TODO: Remove this after https://github.com/Microsoft/TypeScript/issues/2338 lands
-/// <reference path="../node_modules/ts-promise/dist/ts-promise.d.ts" />
-
 /// <reference path="node/node.d.ts" />
